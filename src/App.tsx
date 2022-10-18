1954e056cac301b2390a12b05bb11b89276fdd28
Dhruv Dhamani <dhruv.dhamani@gmail.com>
2022-10-18 16:22:38 -0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import type { Component } from 'solid-js';
+
+const App: Component = () => {
+  return (
+    <input type="text" title="Search" placeholder="Search for anything!" class="w-full py-6 px-3"></input>
+  );
+};
+
+export default App;
